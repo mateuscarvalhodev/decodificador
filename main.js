51313dbc3c4e565324814933e2f1c61b3a2aef93
Mateus Carvalho <mateuscarvalhodev@outlook.com>
2023-01-11 16:16:58 -0300

    desabilitando botão

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+const button = document.getElementById('buttonEncrypt');
+const textArea = document.getElementById('textArea');
 
-// let textForEncrypt;
 function encrypt(textForEncrypt) {
   textForEncrypt = textForEncrypt.replace(/e/g, 'enter')
     .replace(/i/g, 'imes')
@@ -18,24 +19,34 @@ function encrypt(textForEncrypt) {
 
     return textForEncrypt;
   }
-  document.getElementById('textArea').oninput = function() {
+  textArea.oninput = function() {
     textForEncrypt = this.value;
+    if(textForEncrypt.length > 0) {
+      buttonEncrypt.disabled = false;
+      buttonDecrypt.disabled = false;
+    } else {
+      buttonEncrypt.disabled = true;
+      buttonDecrypt.disabled = true;
+    }
     console.log(textForEncrypt);
   const textEncrypted = encrypt(textForEncrypt);
   console.log(textEncrypted)
   ;
   
 }
+
+
 function encryptButton() {
-  textForEncrypt = document.getElementById('textArea').value;
+  textForEncrypt = textArea.value;
   const textEncrypted = encrypt(textForEncrypt);
   document.getElementById("results").innerHTML = '';
   document.getElementById("resultsText").innerHTML = textEncrypted;
   console.log('cliquei');
   
+  
 }
 function decryptButton() {
-  textForEncrypt = document.getElementById('textArea').value;
+  textForEncrypt = textArea.value;
   const textDecrypted = decrypt(textForEncrypt);
   document.getElementById('results').innerHTML = '';
   document.getElementById('resultsText').innerHTML = textDecrypted;
@@ -44,5 +55,5 @@ function decryptButton() {
 
 
 
-console.log(encrypt('toda vez que você me disser oi, eu vou responder só oi'));
-console.log(decrypt('toberdai venterz qufatenter vobercê menter dimesssenterr oberimes, enterufat voberufat renterspoberndenterr só oberimes'));
+// console.log(encrypt('toda vez que você me disser oi, eu vou responder só oi'));
+// console.log(decrypt('toberdai venterz qufatenter vobercê menter dimesssenterr oberimes, enterufat voberufat renterspoberndenterr só oberimes'));
